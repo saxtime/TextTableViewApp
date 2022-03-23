18a8732378e6022a92ad8c0b96a095479ae59e25
saxtime <saxtime@yandex.ru>
2022-03-23 17:52:43 +0300

    Initial Commit

---
+++ TextTableViewDelegateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextTableViewDelegateApp
+//
+//  Created by Андрей Василевский on 3/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
