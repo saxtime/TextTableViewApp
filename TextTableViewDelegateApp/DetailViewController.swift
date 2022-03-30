c2f0c99a1b5a7de28c4bf0bb3e0f96533c7696b2
saxtime <saxtime@yandex.ru>
2022-03-30 20:26:20 +0300

    Added Alert Controller for Empty fields and exit button from detailVC to main

--- TextTableViewDelegateApp/DetailViewController.swift
+++ TextTableViewDelegateApp/DetailViewController.swift
@@ -22,14 +22,25 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
     
-      
+    @IBAction func exitButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func sendButton(_ sender: UIButton) {
         
+        
         guard let name = nameTF.text else { return }
         guard let surname = surnameTF.text else { return }
+        
+        if name == "" {
+            showAlert(with: "Empty field", message: "Please, enter your Name")
+            return
+        } else if surname == "" {
+            showAlert(with: "Empty field", message: "Please, enter your Surname")
+            return
+        }
         
         let person = Person(Name: name, Surname: surname)
         
@@ -40,4 +51,12 @@ class DetailViewController: UIViewController {
         
         dismiss(animated: true, completion: nil)
     }
+    
+    func showAlert(with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
